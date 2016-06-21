57b09fd824efb68a288ca82f4c39c1d315ac57f5
Y.Potupa <y.potupa@gmail.com>
2016-06-21 15:06:46 +0300

    21.06. pulled

--- web/controllers/main_controller.js
+++ web/controllers/main_controller.js
@@ -8,22 +8,13 @@
             //$scope.activePills = $location.url();
             $scope.$on("$viewContentLoaded", function() {
                 var interval = setInterval(function() {
-                    console.log($scope.activePills);
+
                     if ($scope.activePills != "undefined") {
                         $scope.activePills = $location.url();
                         clearInterval(interval);
                     }
                 },1); 
             });
-            ($scope.activatePills = function() {
-            	var interval = setInterval(function() {
-                    console.log($scope.activePills);
-                    if ($scope.activePills != "undefined") {
-                        $scope.activePills = $location.url();
-                        clearInterval(interval);
-                    }
-            	},1);
-            })();   
        }]);
 
 
